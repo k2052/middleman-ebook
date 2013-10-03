628c0fbc0c008c7c0e76b7bf1b84ef038ce43183
K-2052 <k@2052.me>
2013-10-03 10:39:27 -0700

    parsing fixes. added support for asides and textblocks like information, warning etc

--- lib/kramdown/converter/executr.rb
+++ lib/kramdown/converter/executr.rb
@@ -4,9 +4,37 @@ module Kramdown
       def convert_codeblock(el, indent)
         attr = el.attr.dup
         lang = extract_code_language!(attr)
-        result = Pygments.highlight(code, :lexer => lang)
 
-        "#{' '*indent}<pre><code #{html_attributes(attr)} executable='true's>#{result}#{' '*indent}</code></pre>\n"
+        attr["class"] = "" unless attr["class"]
+         
+        if lang == "javascript" or lang == "coffeescript" and attr['executable'] != false
+          attr["class"] << " executable"
+          attr['executable'] = true
+        end
+        
+        attr['data-type'] = lang
+        attr['class'] <<  " language-#{lang}"
+
+        "#{' '*indent}<pre><code #{html_attributes(attr)}>#{el.value}#{' '*indent}</code></pre>\n"
+      end
+
+      def convert_textblock(el, indent)
+        result = "<table class='#{el.attr["textblock_type"]} sidebarish textblock'>"
+        result << "<tbody> <tr>"
+
+        result << "<td class='textblock-sidebar-icon-wrap'><i class='textblock-sidebar-icon icon-#{el.attr["textblock_type"]}'></i></td>"
+        result << "<td>"
+
+        result << inner(el, indent)
+
+        el.attr.delete("textblock_type")
+
+        result << <<-eos      
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        eos
       end
     end
   end
